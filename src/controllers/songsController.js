af215c88d49644b3b53f224b10fd87d387a37658
Catalin Pit <catalinpit@gmail.com>
2021-10-14 15:46:38 +0300

    Getting an individual song works

--- src/controllers/songsController.js
+++ src/controllers/songsController.js
@@ -3,13 +3,22 @@ const pool = require('../db/db');
 const getAllSongs = (req, res) => {
     pool.query(
         'SELECT * FROM songs', (error, results) => {
-            if (error) {
-                throw error;
-            }
+            if (error) throw error;
+
             res.status(200).json(results.rows);
-        })
+        });
+};
+
+const getSong = (req, res) => {
+    pool.query(
+        `SELECT * FROM songs WHERE '${req.params.artist}'=ANY(artists)`, (error, results) => {
+            if (error) throw error;
+
+            res.status(200).json(results.rows);
+        });
 };
 
 module.exports = {
-    getAllSongs
+    getAllSongs,
+    getSong
 };
--- src/routes/songs.js
+++ src/routes/songs.js
@@ -4,7 +4,7 @@ const controller = require('../controllers/songsController.js');
 const router = express.Router();
 
 router.get('/songs', controller.getAllSongs);
-// router.get('/songs/:id', controller.getSong);
+router.get('/songs/:artist', controller.getSong);
 // router.post('/songs', controller.addSong);
 // router.put('/songs/:id', controller.updateSong);
 // router.delete('/songs/:id', controller.deleteSong);
