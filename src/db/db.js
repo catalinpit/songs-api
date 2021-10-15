7535ef368efb6192423cf2bb952fe8ce8002e2e2
Catalin Pit <catalinpit@gmail.com>
2021-10-15 09:05:16 +0300

    Added heroku db details

--- src/db/db.js
+++ src/db/db.js
@@ -7,6 +7,9 @@ const pool = new Pool({
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
     port: process.env.POSTGRES_PORT,
+    ssl: {
+        rejectUnauthorized: false,
+    }
 });
 
 module.exports = pool;
